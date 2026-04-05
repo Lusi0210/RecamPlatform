3f7cdf6c8a490f8a169d001f92b8f9eadad46fdb
Lusi A <z5631341@ad.unsw.edu.au>
2026-04-06 04:30:08 +1000

    feat(RECAM-65): Ensure media API accessible by both Agent and Photography

--- Remp/Remp.API/Controllers/MediaAssetController.cs
+++ Remp/Remp.API/Controllers/MediaAssetController.cs
@@ -21,6 +21,7 @@ namespace Remp.Remp.API.Controllers
         }
 
         [HttpGet("listing/{listingCaseId}")]
+        [Authorize]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status401Unauthorized)]
         [ProducesResponseType(StatusCodes.Status500InternalServerError)]
